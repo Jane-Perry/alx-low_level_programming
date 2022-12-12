43b26f301b0592abbe7beeaa326d18caa0a0fa57
Jane-Perry <jnperry40@gmail.com>
2022-12-12 02:05:02 -0800

    Print code withputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
-
-/*
+/**
  * main - print code using puts
  *
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
