7609a30c675da46490fcf46d95c1744d0c1e85b7
Jane-Perry <jnperry40@gmail.com>
2022-12-12 20:22:50 -0800

    Task 8 re

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Print a line of code
  *
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\"" "- Dora Korpar, 2015-10-19");
 	return (1);
 }
