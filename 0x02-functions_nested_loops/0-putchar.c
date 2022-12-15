a91b35d1b2ff5202c406976330ab4b42cb177663
Jane-Perry <jnperry40@gmail.com>
2022-12-14 17:42:26 -0800

    Bputchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	char test[] = "_putchar";
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(test[c];
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
