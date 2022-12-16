52d1ff5534c66c7dc995533f71a86a7dd1d91820
Jane-Perry <jnperry40@gmail.com>
2022-12-16 04:41:52 -0800

    Display 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,10 @@ void times_table(void)
 
 			product = num * mult;
 			if (product <= 9)
-			{
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}
