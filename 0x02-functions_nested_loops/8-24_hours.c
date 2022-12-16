e91f25b44fc910f798db3f0e81151f6b34997b95
Jane-Perry <jnperry40@gmail.com>
2022-12-15 23:52:50 -0800

    8 Jack Bauer minutes in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,15 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-	}
-	for (minute = 0; minute <= 59; minute++)
-	{
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0');
-		_putchar('\n');
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
