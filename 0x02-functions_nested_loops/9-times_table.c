9f0ed84c67e11837936ba197b51eb7e2efe23070
Jane-Perry <jnperry40@gmail.com>
2022-12-16 04:22:14 -0800

    Task 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,15 +16,18 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num * mult;
+			product = num * mult;
 
 			if
-				(prod <= 9);
+			{
+				(product <= 9);
 				_putchar(' ');
-
+			}
+			{
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
