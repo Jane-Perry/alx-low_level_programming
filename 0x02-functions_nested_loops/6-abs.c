f240b10d940365d615b08a0aadcb2c96b213dc81
Jane-Perry <jnperry40@gmail.com>
2022-12-15 19:08:17 -0800

    Redo task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * int _abs - Calculates the absolute value of an integer
+ * int _abs(int) - Calculates the absolute value of an integer
  * @int: - Represents an integer number
+ * @ _abs: Denotes the total value of an integer
  * Return: Absolute value
  */
 
