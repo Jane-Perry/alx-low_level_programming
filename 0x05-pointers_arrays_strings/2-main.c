fe4dc921bfee97fbee3ad1cbf026e398ba8de539
Jane-Perry <jnperry40@gmail.com>
2022-12-20 00:45:15 -0800

    testtry3

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the sizeof pointer in bytes
+ * Return: 0
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Size of pointer: %lu\n", sizeof(p));
+	return (0);
+}
